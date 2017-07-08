8d78e9a7506fdd8f63ba3182991c3a97511ab9bc
Brennan Baldridge <bbaldridge@moodyprice.com>
2017-07-07 22:14:05 -0500

    added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -4,17 +4,28 @@ require('./config/config');
 // global imports
 const express = require('express');
 const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT;
 
 // app
 var app = express();
-const port = process.env.PORT;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // middleware
 app.use(express.static(publicPath));
 
+// events
+io.on('connection', (socket) => {
+    console.log('New browser connection');
+    socket.on('disconnect', () => {
+        console.log('Browser disconnected');
+    });
+});
+
 // porting
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on Port ${port}`);
 });
