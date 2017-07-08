94b14340d7e8e0f21f7e3bedfc96272f46c753b2
Brennan Baldridge <bbaldridge@moodyprice.com>
2017-07-07 20:46:31 -0500

    added socket.io

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 
 // global imports
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 
