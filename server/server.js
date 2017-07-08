e6184f6bd168e1c68ff516952a0f2aba30e2e9e3
Brennan Baldridge <bbaldridge@moodyprice.com>
2017-07-07 20:10:17 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// requirements
+require('./config/config');
+
+// global imports
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+// app
+var app = express();
+const port = process.env.PORT;
+
+// middleware
+app.use(express.static(publicPath));
+
+// porting
+app.listen(port, () => {
+    console.log(`Server started on Port ${port}`);
+});
